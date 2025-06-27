eef8cb283f37c5b2a28527cd90bcef4f9a9fe2a2
Kai2389 <kaisato2389@gmail.com>
2025-06-28 06:55:21 +0900

    update hero section and added introducing video

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 (function () {
 
+    var video = document.getElementById('introVideo');
+    var playOverlay = document.getElementById('playOverlay');
+    var playButton = document.getElementById('playButton');
+
     /*=====================================
     Sticky
     ======================================= */
@@ -25,26 +29,26 @@
     };
 
     // section menu active
-	function onScroll(event) {
-		var sections = document.querySelectorAll('.page-scroll');
-		var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    function onScroll(event) {
+        var sections = document.querySelectorAll('.page-scroll');
+        var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 
-		for (var i = 0; i < sections.length; i++) {
-			var currLink = sections[i];
-			var val = currLink.getAttribute('href');
-			var refElement = document.querySelector(val);
-			var scrollTopMinus = scrollPos + 73;
-			if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
-				document.querySelector('.page-scroll').classList.remove('active');
-				currLink.classList.add('active');
-			} else {
-				currLink.classList.remove('active');
-			}
-		}
-	};
+        for (var i = 0; i < sections.length; i++) {
+            var currLink = sections[i];
+            var val = currLink.getAttribute('href');
+            var refElement = document.querySelector(val);
+            var scrollTopMinus = scrollPos + 73;
+            if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+                document.querySelector('.page-scroll').classList.remove('active');
+                currLink.classList.add('active');
+            } else {
+                currLink.classList.remove('active');
+            }
+        }
+    };
 
     window.document.addEventListener('scroll', onScroll);
-    
+
     // for menu scroll 
     var pageLink = document.querySelectorAll('.page-scroll');
 
@@ -58,6 +62,33 @@
         });
     });
 
+//This is video part
+
+
+
+    // Play video when play button is clicked
+    playButton.addEventListener('click', function () {
+        video.play();
+        alert("Asdfasdf")
+
+        playOverlay.classList.add('hidden');
+    });
+
+    // Show play overlay when video is paused
+    video.addEventListener('pause', function () {
+        playOverlay.classList.remove('hidden');
+    });
+
+    // Hide play overlay when video starts playing
+    video.addEventListener('play', function () {
+        playOverlay.classList.add('hidden');
+    });
+
+    // Show play overlay when video ends
+    video.addEventListener('ended', function () {
+        playOverlay.classList.remove('hidden');
+    });
+
     "use strict";
 
-}) ();
+})();
